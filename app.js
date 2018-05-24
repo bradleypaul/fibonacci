fb4404fbb4005934ea4174aca52b7d47119b5f9e
Paul Bradley <paul.bradley@metova.com>
2018-05-24 12:59:16 -0500

    Create project files
    Added project files

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let resultButton = document.querySelector('#generate');
+    let inputField = document.querySelector('#input');
+    let result = document.querySelector('#result');
+    resultButton.onclick = function () { 
+        result.value = fibonacci(inputField.value);
+    };
+});
+
+let fibonacci = term => {
+    if (term == 0) {
+        return 0; 
+    } else if(term == 1) {
+        return 1;
+    } else {
+        return fibonacci(term - 1) + fibonacci(term - 2);
+    }
+}
